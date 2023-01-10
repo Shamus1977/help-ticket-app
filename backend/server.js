1e135a66406f29d910742e49d8552cb08fa6791a
Shay Johnson <shay.johnson1@ibm.com>
2023-01-10 10:00:53 -0600

    Added functionality to userController to register/create user in database via a user object

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-console.log("Server........");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const connectDb = require("./config/db");
+const colors = require("colors");
+const PORT = process.env.PORT || 5000;
+const {errorHandler} = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
+//Connect to database
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.get("/", (req,res) => {
+    res.status(200).json({message:"Welcome to the Support Desk API"});
+});
+
+//Routes
+app.use(`/api/users`, require("./routes/userRoutes"));
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
