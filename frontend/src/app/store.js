f48b6a91db8bda59d60c33df0c853beb81e02bba
Shay Johnson <shay.johnson1@ibm.com>
2023-01-10 20:50:19 -0600

    added logout functionality

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from "../features/auth/authSlice";
 
+//REducers are located in the slice files
 export const store = configureStore({
   reducer: {
     auth: authReducer,
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -31,12 +31,17 @@ export const register =  createAsyncThunk("auth/register",
         //Then action can be taken with information.
         // Set user, etc...
     });
-    
+
 //Login user
 export const login =  createAsyncThunk("auth/login",
     async (user, thunkAPI) => {
         console.log(user);
     });
+
+// Logout user
+export const logout = createAsyncThunk("auth/logout", async () => {
+    await authService.logout();
+});
 
 
 
@@ -72,6 +77,9 @@ export const authSlice = createSlice({
                 state.isLoading = false;
                 state.isError = true;
                 state.message = action.payload;
+                state.user = null;
+            })
+            .addCase(logout.fulfilled, (state) => {
                 state.user = null;
             })
     }
