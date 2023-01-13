a80e62ba422648645168e23bfb17656c89d15537
Shay Johnson <shay.johnson1@ibm.com>
2023-01-12 19:51:09 -0600

    added fix for app from githu repo

--- backend/server.js
+++ backend/server.js
@@ -30,11 +30,14 @@ if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/build")));
 
     //Load the html file with the root div
-    app.get("*", (req, res) => res.sendFile(__dirname, "../", "frontend", "build", "index.html"));
-}else {
-    app.get("/", (req,res) => {
-        res.status(200).json({message:"Welcome to the Support Desk API"});
-    });
+  // FIX: below code fixes app crashing on refresh in deployment
+    app.get('*', (_, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+    })
+} else {
+    app.get('/', (_, res) => {
+        res.status(200).json({ message: 'Welcome to the Support Desk API' })
+    })
 }
 
 //Error handliing
